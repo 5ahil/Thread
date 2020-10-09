a5837b0cb371167fa19d0be8d7e95fac0453aea4
Shahil Sabbag <withshahil@gmail.com>
2020-10-09 20:10:27 +0530

    master commit3

--- test.cpp
+++ test.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
    
-   cout<<"Some random statement\n";
+   cout<<"Some random statement1\n";
    cout<<"Some random statement2\n";
+   cout<<"Some random statement3\n";
 }
---
+++ test3.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+   
+   cout<<"Some random statement\n";
+}
